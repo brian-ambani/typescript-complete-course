db7c2fc6f04681c2aee06b450f14eef668e01f42
brian-ambani <brainambani1@gmail.com>
2023-10-30 01:59:08 +0300

    updated the basic types

--- practice-0/dist/main.js
+++ practice-0/dist/main.js
@@ -8,3 +8,23 @@ let ids = [1, 2, 3, 4, 5]; // array of numbers
 let arr = [1, true, "Hello"]; // array of any type
 // Tuple
 let person = [1, "Brain", true]; // tuple
+// Tuple Array
+let employee; // array of tuples
+employee = [
+    [1, "Brain"],
+    [2, "John"],
+    [3, "Jill"],
+];
+// Union
+let pid;
+pid = "22";
+pid = 22;
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Left);
--- practice-0/src/main.ts
+++ practice-0/src/main.ts
@@ -9,3 +9,27 @@ let arr: any[] = [1, true, "Hello"]; // array of any type
 
 // Tuple
 let person: [number, string, boolean] = [1, "Brain", true]; // tuple
+
+// Tuple Array
+let employee: [number, string][]; // array of tuples
+employee = [
+  [1, "Brain"],
+  [2, "John"],
+  [3, "Jill"],
+];
+
+// Union
+let pid: string | number;
+
+pid = "22";
+pid = 22;
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+console.log(Direction1.Left);
