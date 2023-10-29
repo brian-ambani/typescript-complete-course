6041255a4c7d86bf3901249e2e922c8bb6c142dc
brian-ambani <brainambani1@gmail.com>
2023-10-30 01:39:05 +0300

    updated lesson-1

--- lesson-01/build/js/main.js
+++ lesson-01/build/js/main.js
@@ -6,3 +6,13 @@ let b = 6;
 let c = 2;
 console.log(a / b);
 console.log(c / b);
+//tuple
+let firstTuple = ["first", 1, true];
+// object
+let employee = {
+    name: "Brain",
+    age: 25,
+    salary: 1000,
+    isBlack: true,
+};
+console.log(employee);
--- lesson-01/src/main.ts
+++ lesson-01/src/main.ts
@@ -8,3 +8,17 @@ let c: number = 2;
 console.log(a / b);
 
 console.log(c / b);
+
+//tuple
+let firstTuple: [string, number, boolean] = ["first", 1, true];
+
+// object
+let employee: { name: string; age: number; salary: number; isBlack: boolean } =
+  {
+    name: "Brain",
+    age: 25,
+    salary: 1000,
+    isBlack: true,
+  };
+
+console.log(employee);
