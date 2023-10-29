cc4fa8e45930a4eb1e809c84adc26aacac97fbf5
brian-ambani <brainambani1@gmail.com>
2023-10-30 01:56:09 +0300

    updated the basic types

---
+++ practice-0/dist/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Basic types
+let id = 5;
+let company = "Flin Inc";
+let isPublished = true;
+let x = "Hello"; // any type
+let ids = [1, 2, 3, 4, 5]; // array of numbers
+let arr = [1, true, "Hello"]; // array of any type
+// Tuple
+let person = [1, "Brain", true]; // tuple
---
+++ practice-0/src/main.ts
@@ -0,0 +1,11 @@
+// Basic types
+let id: number = 5;
+let company: string = "Flin Inc";
+let isPublished: boolean = true;
+let x: any = "Hello"; // any type
+
+let ids: number[] = [1, 2, 3, 4, 5]; // array of numbers
+let arr: any[] = [1, true, "Hello"]; // array of any type
+
+// Tuple
+let person: [number, string, boolean] = [1, "Brain", true]; // tuple
